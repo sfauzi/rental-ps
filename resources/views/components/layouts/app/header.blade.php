4477e668c69b9c9485773283cf909863e0d3214c
sfauzii <sjahrulfauzii@gmail.com>
2025-03-14 17:27:07 +0700

    fix: :bug: fixed header

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
-
     <head>
         @include('partials.head')
     </head>
-
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
@@ -14,23 +12,20 @@
             </a>
 
             <flux:navbar class="-mb-px max-lg:hidden">
-                @if (Auth::check() && Auth::user()->role === 'admin')
-                    <flux:navbar.item icon="layout-grid" :href="route('dashboard')"
-                        :current="request() - > routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
-                    </flux:navbar.item>
+                @if(Auth::check() && Auth::user()->role === 'admin')
+                <flux:navbar.item icon="layout-grid" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
+                    {{ __('Dashboard') }}
+                </flux:navbar.item>
                 @endif
-                @if (Auth::check() && Auth::user()->role === 'admin')
-                    <flux:navbar.item icon="star" :href="route('services')" :current="request() - > routeIs('services')"
-                        wire:navigate>
+                @if(Auth::check() && Auth::user()->role === 'admin')
+                    <flux:navbar.item icon="star" :href="route('services')" :current="request()->routeIs('services')" wire:navigate>
                         {{ __('Services') }}
                     </flux:navbar.item>
                 @endif
 
-                <flux:navbar.item icon="receipt-percent" :href="route('my-booking')"
-                    :current="request() - > routeIs('my-booking')" wire:navigate>
-                    {{ __('Booking') }}
-                </flux:navbar.item>
+                 <flux:navbar.item icon="receipt-percent" :href="route('my-booking')" :current="request()->routeIs('my-booking')" wire:navigate>
+                        {{ __('Booking') }}
+                 </flux:navbar.item>
             </flux:navbar>
 
             <flux:spacer />
@@ -39,9 +34,14 @@
                 {{-- <flux:tooltip :content="__('Search')" position="bottom">
                     <flux:navbar.item class="!h-10 [&>div>svg]:size-5" icon="magnifying-glass" href="#" :label="__('Search')" />
                 </flux:tooltip> --}}
-                <flux:tooltip :content="__('Repository')" position="bottom">
-                    <flux:navbar.item class="h-10 max-lg:hidden [&>div>svg]:size-5" icon="folder-git-2"
-                        href="https://github.com/sfauzi/rental-ps.git" target="_blank" :label="__('Repository')" />
+                 <flux:tooltip :content="__('Repository')" position="bottom">
+                    <flux:navbar.item
+                        class="h-10 max-lg:hidden [&>div>svg]:size-5"
+                        icon="folder-git-2"
+                        href="https://rental-ps.speechlessmind.com/"
+                        target="_blank"
+                        :label="__('Repository')"
+                    />
                 </flux:tooltip>
                 {{-- <flux:tooltip :content="__('Documentation')" position="bottom">
                     <flux:navbar.item
@@ -56,7 +56,10 @@
 
             <!-- Desktop User Menu -->
             <flux:dropdown position="top" align="end">
-                <flux:profile class="cursor-pointer" :initials="auth() - > user() - > initials()" />
+                <flux:profile
+                    class="cursor-pointer"
+                    :initials="auth()->user()->initials()"
+                />
 
                 <flux:menu>
                     <flux:menu.radio.group>
@@ -64,7 +67,8 @@
                             <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                 <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
                                     <span
-                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
+                                    >
                                         {{ auth()->user()->initials() }}
                                     </span>
                                 </span>
@@ -80,16 +84,14 @@
                     <flux:menu.separator />
 
                     <flux:menu.radio.group>
-                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>
-                            {{ __('Settings') }}</flux:menu.item>
+                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
                     </flux:menu.radio.group>
 
                     <flux:menu.separator />
 
                     <form method="POST" action="{{ route('logout') }}" class="w-full">
                         @csrf
-                        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle"
-                            class="w-full">
+                        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
                             {{ __('Log Out') }}
                         </flux:menu.item>
                     </form>
@@ -98,8 +100,7 @@
         </flux:header>
 
         <!-- Mobile Menu -->
-        <flux:sidebar stashable sticky
-            class="lg:hidden border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+        <flux:sidebar stashable sticky class="lg:hidden border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
             <a href="{{ route('dashboard') }}" class="ml-1 flex items-center space-x-2" wire:navigate>
@@ -108,9 +109,8 @@
 
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')">
-                    <flux:navlist.item icon="layout-grid" :href="route('dashboard')"
-                        :current="request() - > routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
+                    <flux:navlist.item icon="layout-grid" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
+                    {{ __('Dashboard') }}
                     </flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
@@ -118,8 +118,8 @@
             <flux:spacer />
 
             <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/sfauzi/rental-ps.git" target="_blank">
-                    {{ __('Repository') }}
+                <flux:navlist.item icon="folder-git-2" href="https://rental-ps.speechlessmind.com/" target="_blank">
+                {{ __('Repository') }}
                 </flux:navlist.item>
 
                 {{-- <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits" target="_blank">
@@ -132,5 +132,4 @@
 
         @fluxScripts
     </body>
-
 </html>
